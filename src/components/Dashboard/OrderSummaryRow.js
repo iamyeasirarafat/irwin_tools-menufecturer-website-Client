53685982afecf4e7647ce63585f5837352518769
iamyeasirarafat <yas.yeasir@gmail.com>
2022-05-28 15:24:52 +0600

    order delete done

--- src/components/Dashboard/OrderSummaryRow.js
+++ src/components/Dashboard/OrderSummaryRow.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const OrderSummaryRow = ({order}) => {
-    const {name, quantity,client, price}=order
+    const {_id, name, quantity,client, price}=order;
+    const handleCancel = (id) => {
+        const url = `http://localhost:5000/order/${id}`
+        fetch(url,{
+            method: 'DELETE',
+            headers: { 'authorization': `Bearer ${localStorage.getItem('jwtToken')}` }
+        }).then(response => response.json())
+            .then(data => console.log(data))
+        
+    }
     return (
-        <tr className="border-b border-opacity-20 border-gray-700 bg-gray-900">
+                <>
+                <tr className="border-b border-opacity-20 border-gray-700 bg-gray-900">
 					<td className="p-3">
 						<p>{client}</p>
 					</td>
@@ -20,10 +30,21 @@ const OrderSummaryRow = ({order}) => {
 						<p>${Math.round(price)}</p>
 					</td>
 					<td className="p-3 flex text-right">
-						<button className="btn btn-xs mr-3 bg-violet-500">pay</button>
-						<button className="btn btn-xs bg-rose-500">Cancel</button>
+						<button className="btn btn-xs mr-3 bg-violet-500 hover:bg-violet-600">pay</button>
+						
+                        <label for="order-Delete" class="btn btn-xs bg-rose-500 hover:bg-rose-600">Cancel</label>
 					</td>
 				</tr>
+                <input type="checkbox" id="order-Delete" class="modal-toggle" />
+            <div class="modal ">
+                <div class="modal-box relative bg-gray-900">
+                    <label for="order-Delete" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 class="text-lg font-bold text-rose-500">Are you sure want to delete this order?</h3>
+                    <p class="py-4">Its cant be undone!</p>
+                    <button onClick={()=>handleCancel(_id)} className="btn bg-rose-500"><label htmlFor="order-Delete">Delete</label></button>
+                </div>
+            </div>
+                </>
     );
 };
 
